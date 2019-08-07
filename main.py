1b362d444ea7a438f9b42065506c7ef029f7d0ff
Michael Cavusoglu <53866800+mcavusoglu@users.noreply.github.com>
2019-08-07 12:02:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import webapp2
 import jinja2
 import os
 import random
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
